071b164bce4cb88c70dfd213238ff885f6d4ea4f
ibrahimghellab <ghellabibrahim@gmail.com>
2024-11-23 21:21:44 +0100

    First version of the signin page

---
+++ view/signin.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="../public/styles/signin.css">
+</head>
+<body>
+    <h1>Sign-in</h1>
+    <p>Please fill in the information below:</p>
+    <form action="">
+        <input type="text" name="first_name" placeholder="First name" required >
+        <input type="text" name="last_name" placeholder="Last name" required >
+        <input type="text" name="email" placeholder="E-mail" required >
+        <input type="text" name="password" placeholder="Password" required >
+        <input type="submit" value="Create my account">
+    </form>
+</body>
+</html>
